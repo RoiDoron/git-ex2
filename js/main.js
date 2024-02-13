1c0c10635fb4a9655e6c82451ce357f14ddb7f4c
Roi Doron <roidoron6@gmail.com>
2024-02-13 22:25:40 +0200

    Add ball and BallShrink

--- js/main.js
+++ js/main.js
@@ -3,26 +3,49 @@
 
 var gBallSize = 100
 
-function onBallClick(ball){
+function onBallClick(ball) {
     const maxDiameter = ball.dataset.size
-    
-    if(gBallSize<maxDiameter){
-    gBallSize +=getRandomInt(20,60)
-    }else gBallSize = 100
 
-    var innerText=gBallSize
+    if (gBallSize < maxDiameter) {
+        gBallSize += getRandomInt(20, 60)
+    } else gBallSize = 100
 
-    ball.style.width = gBallSize+'px'
-    ball.style.height = gBallSize+'px'
+
+    ball.style.width = gBallSize + 'px'
+    ball.style.height = gBallSize + 'px'
     ball.style.backgroundColor = getRandomColor()
-    
-    ball.innerText=innerText
+
+    ball.innerText = gBallSize
 
 }
 
-function onBallClick2(ball){
-const elBall1 = document.querySelector('div.ball')
-const elBall2 = document.querySelector('div.ball2')
-onBallClick(elBall1)
-onBallClick(elBall2)
+function onBallClick2(ball) {
+    const elBall1 = document.querySelector('div.ball')
+    const elBall2 = document.querySelector('div.ball2')
+    
+    onBallClick(elBall1)
+    onBallClick(elBall2)
+}
+
+function onBallClick3(ball) {
+    const elBall1 = document.querySelector('div.ball')
+    const elBall2 = document.querySelector('div.ball2')
+
+    BallShrink(elBall1)
+    BallShrink(elBall2)
+
+}
+
+function BallShrink(ball) {
+
+    if (gBallSize > 100) {
+        gBallSize -= getRandomInt(20, 60)
+        if (gBallSize < 100) gBallSize = 100
+    }
+
+    ball.style.width = gBallSize + 'px'
+    ball.style.height = gBallSize + 'px'
+
+    ball.innerText = gBallSize
+
 }
