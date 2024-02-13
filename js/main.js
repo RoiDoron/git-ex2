c2322d2d9cfd812e3ee868194bd22039899811d9
Roi Doron <roidoron6@gmail.com>
2024-02-13 16:59:11 +0200

    Make the ball go back to 100 when 400

--- js/main.js
+++ js/main.js
@@ -4,11 +4,15 @@
 var gBallSize = 100
 
 function onBallClick(btn){
+    if(gBallSize<400){
     gBallSize +=50
+    }else gBallSize = 100
+
     var innerText=gBallSize
-    gBallSize +='px'
-    btn.style.width = gBallSize
-    btn.style.height = gBallSize
+
+    btn.style.width = gBallSize+'px'
+    btn.style.height = gBallSize+'px'
+    
     btn.innerText=innerText
 
 }
