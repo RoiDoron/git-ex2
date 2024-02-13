da00bbd98fc238ed0405b888b4cccee7c9750729
Roi Doron <roidoron6@gmail.com>
2024-02-13 16:42:18 +0200

    Infused the obBallClick()

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 'use strict'
 
 
-function onBallClick(){
-    console.log('hi')
+var gBallSize = 100
+
+function onBallClick(btn){
+    gBallSize +=50
+    var innerText=gBallSize
+    gBallSize +='px'
+    btn.style.width = gBallSize
+    btn.style.height = gBallSize
+    btn.innerText=innerText
+
 }
